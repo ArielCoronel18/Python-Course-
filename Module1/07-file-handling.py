403945573f7342f80565e6f49319f7a8eda93cfc
Ariel Coronel <e.arielcoronel@hotmail.com>
2026-04-10 00:30:57 -0600

    Modified the file handling because the files my_fiel.json and my_fiel.csv

--- Module1/07-file-handling.py
+++ Module1/07-file-handling.py
@@ -22,3 +22,55 @@ txt_file.close()
 # Eliminar fichero
 
 #os.remove("Module1/my_file.txt")
+
+# .json file
+
+import json # json es una especie de mapa - un diccionario, crea calve valor
+
+json_file = open("Module1/my_file.json", "w+")
+
+
+json_test = {
+    "Name": "Ariel", 
+    "Surname": "Coronel", 
+    "Age": 35, 
+    "Languages": ["Python", "Swift", "Kotlin"],
+    "WebSite": "https://arielcoro.dev"}
+
+json.dump(json_test, json_file, indent = 2)
+
+json_file.close()
+
+with open("Module1/my_file.json") as my_other_file:
+    for line in my_other_file.readlines():
+        print(line)
+
+json_dict = json.load(open("Module1/my_file.json"))
+print(json_dict)
+print(type(json_dict))  # Ejecuta que es una clase de tipo Diccionario
+print(json_dict["Name"])
+
+# .csv file 
+
+import csv
+
+csv_file = open("Module1/my_file.csv", "w+")
+
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(["Name", "SurName", "Age", "Language", "WebSite"])
+csv_writer.writerow(["Ariel", "Coronel", "37", "Pytho", "https://arielcor.dev"])
+csv_writer.writerow(["Ross", "", "2", "COBOL", ""])
+
+csv_file.close()
+
+with open("Module1/my_file.csv") as my_other_file:
+    for line in my_other_file.readlines():
+        print(line)
+
+# .xlsx file
+
+#import xlrd  # Debe instalarse el modulo xlrd
+
+# .xml
+
+#import xml
